876b2b2087b849e46b1adabe7e4cc4798a21c615
Daniel Agorander <daniel.agorander@gmail.com>
2022-06-13 16:00:22 +0200

    Adding the new stuff from OpenBSD

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Configuring Neovim from scratch
+
+-- NOTES
+-- Packer: https://github.com/wbthomason/packer.nvim
+-- Treesitter? Not sure if just custom: https://github.com/nvim-treesitter/nvim-treesitter
+
+-- Basic commands using basic vim commands
+vim.cmd([[
+	set relativenumber
+]])
+
